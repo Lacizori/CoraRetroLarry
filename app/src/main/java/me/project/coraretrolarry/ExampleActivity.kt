65a6e3c6ff6542b18e00654c43097d01662edcee
Lacizori_HP <lacizroalneugro@gmail.com>
2021-10-07 00:53:15 +0500

    Добавил метод передачи клика на текст в презентер

--- app/src/main/java/me/project/coraretrolarry/ExampleActivity.kt
+++ app/src/main/java/me/project/coraretrolarry/ExampleActivity.kt
@@ -2,10 +2,20 @@ package me.project.coraretrolarry
 
 import android.os.Bundle
 import moxy.MvpAppCompatActivity
+import moxy.presenter.InjectPresenter
+import kotlinx.android.synthetic.main.activity_main.*
 
 class ExampleActivity : MvpAppCompatActivity() {
+
+    @InjectPresenter
+    lateinit var presenter: ExamplePresenter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        textView.setOnClickListener {
+            presenter.onTextClicked()
+
     }
+
 }
