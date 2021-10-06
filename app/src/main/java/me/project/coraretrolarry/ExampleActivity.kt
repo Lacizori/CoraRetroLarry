619cbc2e62c3946d85ecc902e7ed769af61a15d0
Lacizori_HP <lacizroalneugro@gmail.com>
2021-10-07 00:32:17 +0500

    Переименовал файлы, добавил презентер, вью

--- app/src/main/java/me/project/coraretrolarry/ExampleActivity.kt
+++ app/src/main/java/me/project/coraretrolarry/ExampleActivity.kt
@@ -3,7 +3,7 @@ package me.project.coraretrolarry
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class ExampleActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
---
+++ app/src/main/java/me/project/coraretrolarry/ExampleView.kt
@@ -0,0 +1,4 @@
+package me.project.coraretrolarry
+
+class ExampleView {
+}
