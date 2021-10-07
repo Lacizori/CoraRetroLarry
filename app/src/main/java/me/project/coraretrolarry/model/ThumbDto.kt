2f0a177fd60fddcfccd94aa2ff8d81598256a89a
Vortex Alexander <avorteks@alfabank.ru>
2021-10-08 00:41:14 +0300

    Добавил его в dto. Теперь если бы ты хотел - мог бы обратиться к нему thumb.size.value или проверить if (thumb.size == LARGE)

--- app/src/main/java/me/project/coraretrolarry/model/ThumbDto.kt
+++ app/src/main/java/me/project/coraretrolarry/model/ThumbDto.kt
@@ -1,13 +1,12 @@
 package me.project.coraretrolarry.model
 
-
 import com.google.gson.annotations.SerializedName
 
 data class ThumbDto(
     @SerializedName("height")
     val height: Int,
     @SerializedName("size")
-    val size: String,
+    val size: ThumbSize,
     @SerializedName("src")
     val src: String,
     @SerializedName("width")
